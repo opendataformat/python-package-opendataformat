1cb13b71e107a78117a105c85b3ca35f41a6a2ca
xyhdiw <80601447+xyhdiw@users.noreply.github.com>
2024-11-20 10:19:30 +0100

    Update __init__.py

--- opendataformat/__init__.py
+++ opendataformat/__init__.py
@@ -1,5 +1,5 @@
 # xml_dataframe/__init__.py
 
-from .odf2pd import read_odf
-from .pd2odf import write_odf
+from .read_odf import read_odf
+from .write_odf import write_odf
 from .docu_odf import docu_odf
