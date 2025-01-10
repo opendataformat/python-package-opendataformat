1afd0a39a461eaf16dee417bc278668a5722b6c7
thartl-diw <thartl@diw.de>
2025-01-10 11:20:11 +0100

    add author name and email to setup

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='opendataformat',
-    version='0.1',
+    version='0.2',
     packages=find_packages(),
     include_package_data=True,  # Include package data specified in MANIFEST.in
     package_data={
@@ -14,8 +14,8 @@ setup(
         'pandas',
     ],
     description='The Open Data Format (ODF) is a new, non-proprietary, multilingual, metadata enriched, and zip-compressed data format with metadata structured in the Data Documentation Initiative (DDI) Codebook standard. This package allows reading and writing of data files in the Open Data Format (ODF) in R, and displaying metadata in different languages. For further information on the Open Data Format, see <https://opendataformat.github.io/>.',
-    author='Your Name',
-    author_email='your.email@example.com',
+    author='Xiaoyao Han',
+    author_email='xhan@diw.de',
     url='https://github.com/opendataformat/py-package-opendataformat',  # Replace with your GitHub URL
     classifiers=[
         'Programming Language :: Python :: 3',
