38e959f20a69221e729e913726273f287612a84d
xyhdiw <80601447+xyhdiw@users.noreply.github.com>
2025-01-20 10:13:42 +0100

    Update setup.py

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='opendataformat',
-    version='0.2',
+    version='0.1.0',
     packages=find_packages(),
     include_package_data=True,  # Include package data specified in MANIFEST.in
     package_data={
